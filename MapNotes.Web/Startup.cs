21034ff7deee51e2ebd3a84aa0cc8ddb102ad15a
Kirill Starodubtsev <kstarodubtsev@gmail.com>
2016-02-06 18:10:07 +0600

    Initial commit

---
+++ MapNotes.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MapNotes.Web.Startup))]
+namespace MapNotes.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
